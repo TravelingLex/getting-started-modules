9be679fd79bac969aacc648626f89c2b5651b5aa
Alex Meyer <alexmeyer@Alexs-MacBook-Pro.local>
2019-07-09 15:25:09 -0700

    Removed backend from modules.

--- services/webserver-cluster/main.tf
+++ services/webserver-cluster/main.tf
@@ -1,15 +1,3 @@
-terraform {
-  required_version = ">= 0.12, < 0.13"
-  backend "s3"{
-    bucket = "terraforming-up-and-running-state"
-    key = "stage/services/webserver-cluster/terraform.tfstate"
-    region = "us-east-1"
-    dynamodb_table = "terraform-state-lock"
-    profile = "personal"
-    shared_credentials_file = "~/.aws/credentials"
-  }
-}
-
 resource "aws_launch_configuration" "example" {
     image_id = "${var.web_image_id}"
     instance_type = "${var.instance_type}"
