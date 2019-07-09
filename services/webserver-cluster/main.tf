b30fc6fab26d3366d49dcd8c0f47ee29f9dad97b
Alex Meyer <alexmeyer@Alexs-MacBook-Pro.local>
2019-07-09 16:51:17 -0700

    Updated

--- services/webserver-cluster/main.tf
+++ services/webserver-cluster/main.tf
@@ -1,8 +1,8 @@
 resource "aws_launch_configuration" "example" {
-  image_id        = var.image_id
-  instance_type   = var.instance_type
-  security_groups = [aws_security_group.instance.id]
-  user_data       = data.template_file.user_data.rendered
+  image_id        = "${var.image_id}"
+  instance_type   = "${var.instance_type}"
+  security_groups = ["${aws_security_group.instance.id}"]
+  user_data       = "${data.template_file.user_data.rendered}"
 
   lifecycle {
     create_before_destroy = true
@@ -10,14 +10,14 @@ resource "aws_launch_configuration" "example" {
 }
 
 resource "aws_autoscaling_group" "example" {
-  launch_configuration = aws_launch_configuration.example.id
-  availability_zones   = data.aws_availability_zones.all.names
+  launch_configuration = "${aws_launch_configuration.example.id}"
+  availability_zones   = "${data.aws_availability_zones.all.names}"
 
-  load_balancers    = [aws_elb.example.name]
+  load_balancers    = ["${aws_elb.example.name}"]
   health_check_type = "ELB"
 
-  min_size = var.min_size
-  max_size = var.max_size
+  min_size = "${var.min_size}"
+  max_size = "${var.max_size}"
 
   tag {
     key                 = "Name"
@@ -28,13 +28,13 @@ resource "aws_autoscaling_group" "example" {
 
 resource "aws_elb" "example" {
   name               = "${var.cluster_name}-elb"
-  availability_zones = data.aws_availability_zones.all.names
-  security_groups    = [aws_security_group.elb.id]
+  availability_zones = "${data.aws_availability_zones.all.names}"
+  security_groups    = ["${aws_security_group.elb.id}"]
 
   listener {
     lb_port           = 80
     lb_protocol       = "http"
-    instance_port     = var.server_port
+    instance_port     = "${var.server_port}"
     instance_protocol = "http"
   }
 
@@ -51,8 +51,8 @@ resource "aws_security_group" "instance" {
   name = "${var.cluster_name}-instance"
 
   ingress {
-    from_port   = var.server_port
-    to_port     = var.server_port
+    from_port   = "${var.server_port}"
+    to_port     = "${var.server_port}"
     protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
@@ -64,7 +64,7 @@ resource "aws_security_group" "elb" {
 
 resource "aws_security_group_rule" "allow_http_inbound" {
   type              = "ingress"
-  security_group_id = aws_security_group.elb.id
+  security_group_id = "${aws_security_group.elb.id}"
 
   from_port   = 80
   to_port     = 80
@@ -74,7 +74,7 @@ resource "aws_security_group_rule" "allow_http_inbound" {
 
 resource "aws_security_group_rule" "allow_all_outbound" {
   type              = "egress"
-  security_group_id = aws_security_group.elb.id
+  security_group_id = "${aws_security_group.elb.id}"
 
   from_port   = 0
   to_port     = 0
@@ -89,8 +89,8 @@ data "terraform_remote_state" "db" {
   backend = "s3"
 
   config = {
-    bucket                  = var.db_remote_state_bucket
-    key                     = var.db_remote_state_key
+    bucket                  = "${var.db_remote_state_bucket}"
+    key                     = "${var.db_remote_state_key}"
     region                  = "us-east-1"
     profile                 = "personal"
     shared_credentials_file = "~/.aws/credentials"
@@ -98,12 +98,12 @@ data "terraform_remote_state" "db" {
 }
 
 data "template_file" "user_data" {
-  template = file("${path.module}/user-data.sh")
+  template = "${file("${path.module}/user-data.sh")}"
 
   vars = {
-    server_port = var.server_port
-    db_address  = data.terraform_remote_state.db.outputs.address
-    db_port     = data.terraform_remote_state.db.outputs.port
+    server_port = "${var.server_port}"
+    db_address  = "${data.terraform_remote_state.db.outputs.address}"
+    db_port     = "${data.terraform_remote_state.db.outputs.port}"
   }
 }
 
